1d3937b79ae9e228d104ae7f00bb86bec4c76acf
Magdi-Khaled <migokhaled88@gmail.com>
2025-06-28 15:11:42 +0300

    chore: handle cart item add to cart/increase order quantity

--- components/Collection/Product/Quantity.vue
+++ components/Collection/Product/Quantity.vue
@@ -14,17 +14,13 @@ const decreaseQuantity = () => selectedQuantity.value > 1 ? selectedQuantity.val
 <template>
     <div class="grid grid-cols-3 border-[2px] border-hover"
         :class="{ 'w-[120px] my-2': !isCart, 'w-[90px] text-sm': isCart }">
-        <button @click="increaseQuantity"
+        <button @click="isCart ? useCartStore().addToCart(item) : increaseQuantity()"
             class="cursor-pointer flex items-center justify-center w-full h-full hover:bg-hover hover:text-neutral-50 transition-all">
             <Icon name="line-md:plus" />
         </button>
         <p class="border-x-[2px] border-x-hover py-px text-center" :class="{ 'font-semibold': !isCart }">
             {{ selectedQuantity }}</p>
-        <button v-if="isCart" @click="useCartStore().removeFromCart(item)"
-            class="cursor-pointer flex items-center justify-center w-full h-full hover:bg-hover hover:text-neutral-50 transition-all">
-            <Icon name="line-md:minus" />
-        </button>
-        <button v-else @click="decreaseQuantity"
+        <button @click="isCart ? useCartStore().removeFromCart(item) : decreaseQuantity()"
             class="cursor-pointer flex items-center justify-center w-full h-full hover:bg-hover hover:text-neutral-50 transition-all">
             <Icon name="line-md:minus" />
         </button>
